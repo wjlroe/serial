87f24a0d5c71b43cae2ecee51091066aab439c76
William Roe <git@wjlr.org.uk>
2016-12-06 23:36:24 +0000

    Update deps

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [overtone "0.9.1"]
-                 [leipzig "0.8.1"]
+                 [overtone "0.10.1"]
+                 [leipzig "0.10.0"]
                  [enlive "1.1.5"]
                  ]
   ;;  :main serial.twelve-tone
